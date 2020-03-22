01c355ac208ba29a4a3082e9706da29e71cd9ce0
Seth Torralba <sethosrictorralba@gmail.com>
2020-03-22 12:28:47 -0500

    Initial Commit

---
+++ TicTacToeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToeProject
+//
+//  Created by Seth Osric Torralba on 3/22/20.
+//  Copyright © 2020 Seth Osric Torralba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
